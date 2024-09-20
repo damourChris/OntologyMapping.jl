87f6898ce4ac85b167b465da39eac1b96d042404
Documenter.jl <documenter@juliadocs.github.io>
2024-09-20 21:20:39 +0000

    build based on 786a29e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OntologyMapping","category":"page"},{"location":"#OntologyMapping","page":"Home","title":"OntologyMapping","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OntologyMapping.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OntologyMapping]","category":"page"}]
+}
